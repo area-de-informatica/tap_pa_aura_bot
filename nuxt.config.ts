337c571ddb6b98d0a267a90e01a9628dc05e2259
JESULIKE <jgonzalezhernandez41@correo.unicordoba.edu.co>
2025-06-12 15:48:43 -0500

    Add new html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  
+  ssr: false,
+  app: {
+    baseURL: '/tap_pa_aura_bot/', // MUY IMPORTANTE para GitHub Pages
+  },
+
   build: {
     transpile: ['vuetify'],
   },
